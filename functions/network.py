897769589f626ff5783a7719b1dfc64f9f10d608
specfazhou <86537418+specfazhou@users.noreply.github.com>
2023-05-19 14:11:10 -0500

    just to remember it

--- functions/network.py
+++ functions/network.py
@@ -38,4 +38,61 @@ class Net(nn.Module):
 
     
 
+# this is the class for ResNet with only fully connected layers
+class ResidualBlock(nn.Module):
+    def __init__(self, in_features, out_features, activation=nn.ReLU()):
+        super(ResidualBlock, self).__init__()
+
+        self.fc1 = nn.Linear(in_features, out_features)
+        self.fc2 = nn.Linear(out_features, out_features)
+        self.activation = activation
+
+    def forward(self, x):
+        residual = x
+
+        out = self.fc1(x)
+        out = self.activation(out)
+        out = self.fc2(out)
+
+        out += residual
+        out = self.activation(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, input_size, num_classes=10):
+        super(ResNet, self).__init__()
+
+        self.fc1 = nn.Linear(input_size, 64)
+        self.layer1 = self.make_layer(64, 2)
+        self.layer2 = self.make_layer(64, 2)
+        self.layer3 = self.make_layer(64, 2)
+        self.fc2 = nn.Linear(64, num_classes)
+        self.activation = nn.ReLU()
+
+    def make_layer(self, out_features, num_blocks):
+        layers = []
+        for _ in range(num_blocks):
+            layers.append(ResidualBlock(out_features, out_features, nn.ReLU()))
+        return nn.Sequential(*layers)
+
+    def forward(self, t, S, q):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        x = torch.tensor([t, S, q], device = device)
+        out = self.fc1(x)
+        out = self.activation(out)
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+
+        out = self.fc2(out)
+
+        return out
+
+
+
+# this is the Resnet with convolution layers
+
 
